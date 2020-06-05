5a483ad92f261f6e2e172b808341d06546c3bd3c
vivekanandhanv <vvivekvelu@gmail.com>
2020-06-05 12:33:08 +0530

    05062020

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,3 +78,17 @@ $('#submit-form').on('click', function(e) {
     // do something
   );
 })
+
+
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:2,
+        itemsDesktop:[1000,2],
+        itemsDesktopSmall:[979,2],
+        itemsTablet:[768,1],
+        pagination:false,
+        navigation:true,
+        navigationText:["",""],
+        autoPlay:true
+    });
+});
